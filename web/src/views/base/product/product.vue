14a31744ef57fcb7d46a9c670a4e46f13e9f579e
liukexuan <1104390523@qq.com>
2023-01-19 20:30:37 +0800

    2023-01-19

--- web/src/views/base/product/product.vue
+++ web/src/views/base/product/product.vue
@@ -1,22 +1,49 @@
 <template>
-  <el-table-v2
-      :width="width"
-      :height="height"
-      :data="data"
-  >
+  <div class="handle-box">
+    <el-select v-model="data.query.address" placeholder="地址" class="handle-select mr10">
+      <el-option key="1" label="广东省" value="广东省"></el-option>
+      <el-option key="2" label="湖南省" value="湖南省"></el-option>
+    </el-select>
+    <el-input v-model="data.query.name" placeholder="用户名" class="handle-input mr10"></el-input>
+    <el-button type="primary" icon="Search">搜索</el-button>
+    <el-button type="primary" icon="Plus">新增</el-button>
+  </div>
+
+
+  <el-table :data="data.data">
     <el-table-column prop="id" label="id" width="180"/>
-  </el-table-v2>
+  </el-table>
 </template>
 
 <script setup>
 import {reactive} from "vue";
 
-const data = reactive([{
-  id: 1
-}])
+const data = reactive({
+  data: [],
+  query: {
+    name: '11',
+    address: '11'
+  }
+})
+
 
 </script>
 
 <style scoped>
+.handle-box {
+  margin-bottom: 20px;
+}
+
+.handle-select {
+  width: 120px;
+}
+
+.handle-input {
+  width: 300px;
+}
+
+.mr10 {
+  margin-right: 10px;
+}
 
 </style>
